8ce6ef641df8ff80214001eb2e105e8feea2dc4d
rohanghorpade9000 <122144411+rohanghorpade9000@users.noreply.github.com>
2025-02-02 21:03:33 +0530

    Delete redis_client.py

--- redis_client.py
+++
@@ -1,8 +0,0 @@
-import redis
-
-# Initialize Redis client
-redis_client = redis.StrictRedis(
-    host="localhost",
-    port=6379,
-    decode_responses=True  # Ensures results are returned as strings
-)
